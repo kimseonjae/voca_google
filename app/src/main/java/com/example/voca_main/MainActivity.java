61b16ed14305d26ba8fb6788e4217fb20843a50e
seon jae <rlatjswowkd@gmail.com>
2019-06-02 18:38:43 +0900

    기존 + google login

--- app/src/main/java/com/example/voca_main/MainActivity.java
+++ app/src/main/java/com/example/voca_main/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;dh
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.ErrorCodes;
